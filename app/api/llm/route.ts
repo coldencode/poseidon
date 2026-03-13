7a691d2d61178dd10dc75f726cb240e58345bd46
elim0092 <elim0092@student.monash.edu>
2026-03-13 21:27:32 +1100

    update the askGemini

--- app/api/llm/route.ts
+++ app/api/llm/route.ts
@@ -5,14 +5,35 @@ import { NextResponse } from "next/server";
 const genAI = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY});
 
 /**
- * 
- * @param prompt
- * @returns the output of Gemini
+ * Compare user skeleton and chosen
+ * @param user_skeleton
+ * @returns the output of Gemini that give instruction to match the target
  */
-async function askGemini(prompt: string) {
+async function askGemini(user_skeleton: string, chosen_skeleton: string) {
+
+  const finalPrompt = `
+    You are an expert in human pose.
+
+    Both skeletons are in BODY_25 format (25 keypoints with x,y,confidence).
+    Compare the following two skeleton definitions.
+
+    User Skeleton:
+    ${user_skeleton}
+
+    Target Skeleton:
+    ${chosen_skeleton}
+
+    Give ONE short sentence describing how the user skeleton can be improved to better match the target skeleton.
+
+    Rules:
+    - Only output one sentence.
+    - Keep it simple and clear.
+    - Do not include explanations
+    `;
+
   const result = await genAI.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: prompt,
+    contents: finalPrompt,
   });
 
   const text = result.text;
@@ -26,17 +47,17 @@ async function askGemini(prompt: string) {
  * @returns a json containing the the output of Gemini
  */
 export async function POST(req: Request) {
-  const { prompt } = await req.json();
+  const { user_skeleton, chosen_skeleton } = await req.json();
 
-  if (!prompt) {
+  if (!user_skeleton || !chosen_skeleton) {
     return NextResponse.json({
       success: false,
-      error: "Prompt is required",
+      error: "user skeleton and chosen skeleton is required",
     });
   }
 
   try {
-    const response = await askGemini(prompt);
+    const response = await askGemini(user_skeleton, chosen_skeleton);
     return NextResponse.json(response);
   } catch (e) {
     return NextResponse.json({
