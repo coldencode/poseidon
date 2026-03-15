d818ce884a46155f4571499262859e1ebe8db5fa
elim0092 <elim0092@student.monash.edu>
2026-03-15 12:19:28 +1100

    fix imports from changed pose name

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,20 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
 import Results from "./results";
 
-import pose1 from "../../public/pose-library/pose1.json";
-import pose2 from "../../public/pose-library/pose2.json";
+export default async function ResultsPage() {
+  const pose1Path = path.join(process.cwd(), "public/pose-library/ankle_hurt_pose.json");
+  const pose2Path = path.join(process.cwd(), "public/pose-library/baddie_pose.json");
 
-import photo1 from "../../public/pose-library/pose1.png";
-import photo2 from "../../public/pose-library/pose2.png";
+  const [pose1, pose2] = await Promise.all([
+    fs.readFile(pose1Path, "utf8").then((csv) => JSON.parse(csv)),
+    fs.readFile(pose2Path, "utf8").then((csv) => JSON.parse(csv)),
+  ]);
 
-export default function ResultsPage() {
+  const photo1 = "/pose-library/ankle_hurt_pose.png";
+  const photo2 = "/pose-library/baddie_pose.png";
+
   return (
     <>
-    <Results
-      pose={pose2}
-      referencePose={pose1}
-      photo={photo2}
-      referencePhoto={photo1}
-    />
+      <Results
+        pose={pose2}
+        referencePose={pose1}
+        photo={photo2}
+        referencePhoto={photo1}
+      />
     </>
   );
 }
